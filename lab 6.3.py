1a8c5f58f88cbb527cf0b8368d3fd9635a3a611f
4lo8ecK <4lo8ecK@vk.com>
2025-10-08 11:07:04 +0300

    сделано 3-е задание 6 лабораторной работы

---
+++ lab 6.3.py
@@ -0,0 +1,46 @@
+# Лабораторная работа №6
+# Вариант 6
+# Гаврилов Павел ФМ 11-25
+
+# Задание 3
+
+import os
+
+b1 = 0
+
+def main():
+    num = input('Введите число больше нуля: ')
+    if num == 'exit' or num == '': exit(0)
+
+    try:
+        num = int(num)
+
+    except ValueError:
+        print('Нужно ввести число!')
+
+    finally:
+
+        print(f"Первые {num} элементов последовательности:\n-> ", end='')
+
+        if num > 0:
+            prev_value = b1
+            print(b1, end=' ')
+
+            for i in range(1, num):
+                res = 0
+                if i % 2 == 0:
+                    res = prev_value + 3
+                    prev_value = res
+                else:
+                    res = 2 * (prev_value + 3)
+                    prev_value = res
+
+                print(res, end=' ')
+        
+        else:
+            print("Число должно быть больше нуля!")
+while True:
+    os.system('cls')
+    main()
+    cmd = input('\n')
+    if cmd == 'exit': exit(0)
