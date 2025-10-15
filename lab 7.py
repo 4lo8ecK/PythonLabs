bcc87eb1590903a49651a77077434aca9a58c804
4lo8ecK <4lo8ecK@vk.com>
2025-10-15 12:55:44 +0300

    Доделана 7 лабораторка

--- lab 7.py
+++ lab 7.py
@@ -57,29 +57,86 @@ def task_2():
 
 # Задание 3
 def task_3():
+    clear_console()
     print("Задание 3")
     print("Возвращает количество прописных латинских символов")
 
     input_string = input("Ввод: ")
     big_sym_count = 0
     for i in range(0, len(input_string)):
-        if input_string[i] in range(65, 91):
+        if ord(input_string[i]) in range(65, 91):
             big_sym_count += 1
 
     print(f"Ответ - {big_sym_count}")
-    input()
 
 # Задание 4
 def task_4():
     clear_console()
     input_string = input("Введите строку: ")
-    number = int(input("Введите число: "))
+    number = input("Введите число: ")
+    if number.isnumeric():
+        number = int(number)
+    else:
+        return False
 
     if len(input_string) > number:
         print(input_string[(len(input_string) - number)::])
     elif len(input_string) < number:
         print( str('.' * (number - len(input_string))) + str(input_string))
-    input()
 
 # Задание 5
-clear_console()
+def task_5():
+    clear_console()
+    print("Задание 7")
+    print("Смещение символов определённого промежутка в право на один")
+    word = input("Введите само слово/текст:\n-> ")
+    s = input("Верхняя граница: ")
+    k = input("Нижняя граница: ")
+    if k.isnumeric() and s.isnumeric():
+        k = int(k)
+        s = int(s)
+    else:
+        print("Границы должны определяться числами")
+        return False
+
+    if not(0 <= s <= len(word)) or not(0 <= k <= len(word)):
+        print("Промежуток выходит за пределы границ слова")
+        return False
+    if s <= k:
+        print("Верхняя граница не может быть меньше нижней")
+        return False
+
+    res = list(word)
+    for i in range(k-1, s):
+        res[i] = word[i - 1]
+    res[k-1] = word[s-1]
+    
+    res = str(''.join(str(x) for x in res))
+    print(res)
+    return True
+
+def main():
+    clear_console()
+    print("Лабораторная работа №7")
+    print("Выберите задание от 1 до 5")
+    cmd = input()
+    if cmd == 'exit' or cmd == '':
+        exit(0)
+    if not cmd.isnumeric():
+        print("Команда должна быть числом")
+    else:
+        cmd = int(cmd)
+        if 1 <= cmd <= 5:
+            match cmd:
+                case 1: task_1()
+                case 2: task_2()
+                case 3: task_3()
+                case 4: task_4()
+                case 5: task_5()
+        else:
+            print("Неизвестная команда")
+
+while True:
+    main()
+    cmd = input()
+    if (cmd == 'exit'): exit(0)
