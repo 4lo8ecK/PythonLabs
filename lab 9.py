cceaa60e95e51a4ecb8c628f5a0de1b7af811619
4lo8ecK <4lo8ecK@vk.com>
2025-11-03 14:48:05 +0300

    НАчата 9-ая лабораторка: сделаны 1 и 3 задания

---
+++ lab 9.py
@@ -0,0 +1,72 @@
+# Лабораторная работа №9
+# Вариант 6
+# Гаврилов Павел ФМ-11-25
+
+# для генерации случайных списков
+from random import seed, randint
+from math import sqrt
+
+# Очистка консоли (новый вариант через Backspace-последовательность)
+def clear_console() -> None:
+    print("\033c", end="")
+# !clear_console() -> None
+
+# Генерация списка размером в lst_len и со случайными значениями в диапазоне от 0 до lst_len
+def rand_list(elem_count: int, _seed: int = 0, min_value: int = -10, max_value: int = 10) -> list:
+    seed(_seed)
+    return [randint(min_value, max_value) for _ in range(elem_count)]
+# !rand_list(int, int) -> list
+
+def task_1() -> None:
+    print("Задание 1")
+    print("Программа выводит сумму элементов массива с индексами квадратов натуральных чисел")
+    print("и меньше квадратного корня длины массива\n")
+
+    arr_len = input("Введите длину массива для генерации: ")
+    if arr_len == '': return
+    if arr_len == 'exit': exit(0)
+
+    try:
+        arr_len = int(arr_len)
+    except ValueError:
+        print("Нужно ввести число!")
+        input("Нажмите [Enter]")
+        return
+
+    arr = rand_list(arr_len, 0, 0, 100)
+    arr_len_sqrt = sqrt(arr_len)
+
+    print(arr)
+    res = 0
+    for i in range(0, arr_len):
+        sq_i = i ** 2
+        if sq_i < arr_len_sqrt:
+            res += arr[sq_i]
+
+    print(res)
+# !task_1() -> None
+
+def task_3() -> None:
+    print("Задание 3")
+    print("Эта программа выводит произведение наибольшего элемента из первого массива и наименьшего из второго")
+    arr_len = input("Введите длину массива: ")
+
+    if arr_len == '': return
+    if arr_len == 'exit': exit(0)
+
+    try:
+        arr_len = int(arr_len)
+
+    except ValueError:
+        print("Нужно ввести число!")
+        input("Нажмите [Enter]")
+        return
+    
+    arr_a = rand_list(arr_len, 1, -100, 100)
+    arr_c = rand_list(arr_len, 2, -100, 100)
+
+    print(f"Первый массив:\n-> {arr_a}")
+    print(f"Второй массив:\n-> {arr_c}")
+    
+    print("\nРезультат:\n-> ",max(arr_a) * min(arr_c))
+# !task_3() -> None
