1307143dd4941fc71fe32532b47bb236e67cd5e1
4lo8ecK <4lo8ecK@vk.com>
2025-11-12 16:10:25 +0300

    Завершена 11 лаба

--- lab 11.py
+++ lab 11.py
@@ -2,6 +2,59 @@
 # Гаврилов Павел ФМ-11-25
 # Вариант 6
 
+EXIT_KEYS = ['exit', 'q']
+
 # Задание 1
+_dict = {
+    'судно 1': 'рыба',
+    'судно 2': 'не рыба, а мясо',
+    'судно 3': 'техника',
+    'судно 4': 'футбольные мячи',
+    'судно 5': 'бутсы',
+    'судно 6': 'обычная обувь',
+    'судно 7': 'всё подряд'}
 
+def task_1() -> None:
+    print(_dict)
 
+# Задание 2
+def task_2() -> None:
+    keys = _dict.items()
+    for i in keys:
+        print(i)
+    cmd = input("-> ")
+    print(_dict.get(cmd, "Нет такого варианта"))
+# !task_2() -> None
+
+def task_3() -> None:
+    keys = list(_dict.keys())
+    items = list(_dict.values())
+
+    res = {}
+    for i in range(len(keys)):
+        res[items[i]] = keys[i]
+    
+    print(res)
+# !task_3() -> None
+
+# main loop
+while True:
+    print("\033c", end='')
+    print("Лабораторная работа №11")
+    print("Выберите задание")
+    cmd = input("-> ")
+    if cmd in EXIT_KEYS:
+        print("\033c", end='')
+        exit(0)
+    try:
+        cmd = int(cmd)
+    except:
+        continue
+
+    print("\033c", end='')
+    match cmd:
+        case 1: task_1()
+        case 2: task_2()
+        case 3: task_3()
+    cmd = input("-> ")
+    if cmd in EXIT_KEYS: exit(0)
