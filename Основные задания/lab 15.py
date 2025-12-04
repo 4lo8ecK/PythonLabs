bc5c4fca3549645589e3c4197a0ec0d6354bd266
4lo8ecK <4lo8ecK@vk.com>
2025-12-05 00:18:07 +0300

    завершена 15-ая лабораторка

--- Основные задания/lab 15.py
+++ Основные задания/lab 15.py
@@ -2,17 +2,21 @@
 # Вариант 6
 # Гаврилов Павел ФМ-11-25
 
+import os
+
 COMMANDS_STR = ['DEPOSIT', 'WITHDRAW', 'BALANCE', 'TRANSFER', 'INCOME']
 ERR_MSG = 'ERROR'
+
 
 DIR_NAME = 'lab15-files'
 DATA_BASE_FILE = 'comands'
 
-
 g_clients = {}
 
+#region MAIN METHODS
 def deposit(client: str, balance: int = 0) -> bool:
     try:
+        balance = int(balance)
         g_clients.update({client: balance})
         return True
     except:
@@ -21,6 +25,7 @@ def deposit(client: str, balance: int = 0) -> bool:
 
 def withdraw(client: str, summ: int = 0) -> bool:
     try:
+        summ = int(summ)
         balance = g_clients.get(client)
         balance -= summ
         g_clients.update({client: balance})
@@ -38,6 +43,7 @@ def balance(client: str) -> int:
 
 def transfer(name1: str, name2: str, summ: int) -> bool:
     try:
+        summ = int(summ)
         clients_list = list(g_clients.keys())
         if name1 not in clients_list:
             deposit(name1)
@@ -57,34 +63,76 @@ def transfer(name1: str, name2: str, summ: int) -> bool:
 
 def income(proc: int) -> bool:
     try:
+        proc = int(proc)
         clients = list(g_clients.keys())
         for c in clients:
             bal = balance(c)
-            bal = bal * (1 + proc/100)
+            bal = int(bal * (1 + proc/100))
             g_clients.update({c: bal})
         return True
     except:
         return False
 # !income(int) -> bool
-
-def get_commands_list() -> list:
+#endregion
+#region INIT
+def read_file() -> list:
     cmdlines = []
     with open(file=os.path.join(DIR_NAME, DATA_BASE_FILE), mode='r', encoding='utf-8') as file:
         cmdlines = file.readlines()
+    return cmdlines
 
-    
-    
-    
-    
-    
-    pass
+def init_prog(commands: list = []) -> None:
+    if not os.path.exists(DIR_NAME):
+        os.mkdir(DIR_NAME)
+    for i in range(len(commands)):
+        commands[i] += '\n'
+    with open(file=os.path.join(DIR_NAME, DATA_BASE_FILE), mode='w', encoding='utf-8') as file:
+        file.writelines(commands)
+#endregion
+#region PARCER
+def exec_comands() -> None:
+    for _cmd in read_file():
+        if _cmd == '': continue
+        _cmd = _cmd.split()
+        if _cmd[0] in COMMANDS_STR:
+            # deposit
+            if _cmd[0] == COMMANDS_STR[0]:
+                deposit(_cmd[1], int(_cmd[2]))
+            # withdraw
+            elif _cmd[0] ==  COMMANDS_STR[1]:
+                withdraw(_cmd[1], int(_cmd[2]))
+            # balance
+            elif _cmd[0] == COMMANDS_STR[2]:
+                bal = balance(_cmd[1])
+                if bal == None:
+                    print(ERR_MSG)
+                else:
+                    print(bal)
+            # transfer
+            elif _cmd[0] == COMMANDS_STR[3]:
+                transfer(_cmd[1], _cmd[2], int(_cmd[3]))
+            # income
+            elif _cmd[0] == COMMANDS_STR[4]:
+                income(int(_cmd[1]))
+            else:
+                continue
+# deposit('Ivanov', 100)
+# income(5)
+# print(balance('Ivanov'))
+# transfer('Ivanov', 'Petrov', 50)
+# withdraw('Petrov', 100)
+# print(balance('Petrov'))
+# print(balance('Sidorov'))
+#endregion
 
-deposit('Ivanov', 100)
-income(5)
-print(balance('Ivanov'))
-transfer('Ivanov', 'Petrov', 50)
-withdraw('Petrov', 100)
-print(balance('Petrov'))
-print(balance('Sidorov'))
-
-
+commands = [
+    'DEPOSIT Ivanov 100',
+    'INCOME 5',
+    'BALANCE Ivanov',
+    'TRANSFER Ivanov Petrov 50',
+    'WITHDRAW Petrov 100',
+    'BALANCE Petrov',
+    'BALANCE Sidorov'
+]
+init_prog(commands)
+exec_comands()
